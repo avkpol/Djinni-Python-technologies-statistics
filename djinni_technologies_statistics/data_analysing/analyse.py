7ca0d4f50d3a6958f6ba8fafd5bb591a78d9efa2
Andriy <andriykrav@gmail.com>
2023-07-17 15:41:33 +0300

    upgrade analyse.py(add perform_correlation_analysis)

--- djinni_technologies_statistics/data_analysing/analyse.py
+++ djinni_technologies_statistics/data_analysing/analyse.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def analyze_skills():
@@ -52,7 +53,45 @@ def analyze_applicants():
     plt.show()
 
 
+def preprocess_salary(salary):
+    if pd.notnull(salary):
+        if '-' in salary:
+            salary_range = salary.strip().split('-')
+            salary_values = [
+                int(value.replace('$', '').
+                    replace(',', '').strip()[-4:]) for value in salary_range
+            ]
+            return max(salary_values)
+        elif salary.startswith('до'):
+            return int(
+                salary.strip().replace('до $', '').replace(',', '').strip()[-4:]
+            )
+        elif salary.startswith('від'):
+            return int(
+                salary.strip().replace('від $', '').replace(',', '').strip()[-4:]
+            )
+        else:
+            return int(salary.strip().replace('$', '').replace(',', '').strip()[-4:])
+    return 0
+
+def perform_correlation_analysis(vacancies_file, skills_file):
+    df_vacancies = pd.read_csv(vacancies_file)
+    df_skills = pd.read_csv(skills_file)
+    df_vacancies['salary'] = df_vacancies['salary'].apply(preprocess_salary)
+    columns = ['experience', 'salary', 'views', 'applications']
+    for skill in df_skills['Skill']:
+        skill_vacancies = (
+            df_vacancies[df_vacancies['description'].
+            str.contains(skill, case=False, na=False)]
+        )
+        correlation_matrix = skill_vacancies[columns].corr()
+        print(f"Correlation Matrix for Skill: {skill}")
+        print(correlation_matrix)
+        print("\n")
+
+
 
 if __name__ == "__main__":
     analyze_skills()
     analyze_applicants()
+    perform_correlation_analysis('../data/python_vacancies.csv', '../data/skills.csv')
